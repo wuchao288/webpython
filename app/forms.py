b4dd88d2a205d517b8815f9c404920f0c74eddad
wuchao288 <371126627@qq.com>
2018-08-12 16:33:01 +0800

    55eee

--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired,Email
 
+
 class LoginForm(FlaskForm):
     email=StringField('email',validators=[DataRequired(),Email()])
     pwd=PasswordField('pwd',validators=[DataRequired()])
--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 CSRF_ENABLED = True #配置是为了激活 跨站点请求伪造 保护。在大多数情况下，你需
-SECRET_KEY = '12345678' #一个加密的令牌
+SECRET_KEY = '12345678' #一个加密的令
 OPENID_PROVIDERS = [
     { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
     { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
